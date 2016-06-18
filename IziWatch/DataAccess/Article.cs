0b00ec56c5d94bb8a0648951aa13ca61b3860bfb
Fantin VERPILLOT <fantin.verpillot@epita.fr>
2016-06-18 19:13:31 +0200

    DA Article

--- IziWatch/DataAccess/Article.cs
+++ IziWatch/DataAccess/Article.cs
@@ -7,5 +7,132 @@ namespace IziWatch.DataAccess
 {
     public class Article
     {
+        public static bool CreateArticle(DBO.Article article)
+        {
+            using (IziWatchEntities bdd = new IziWatchEntities())
+            {
+                try
+                {
+                    T_Article t_article = new T_Article()
+                    {
+                        id = article.Id,
+                        title = article.Title,
+                        image = article.Image,
+                        category_id = article.CategoryId
+                    };
+                    bdd.T_Article.Add(t_article);
+                    bdd.SaveChanges();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static bool DeleteArticle(long id)
+        {
+            using (IziWatchEntities bdd = new IziWatchEntities())
+            {
+                try
+                {
+                    bdd.T_Article.Remove(bdd.T_Article.Where(x => x.id == id).FirstOrDefault());
+                    bdd.SaveChanges();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static bool UpdateArticle(DBO.Article article)
+        {
+            using (IziWatchEntities bdd = new IziWatchEntities())
+            {
+                try
+                {
+                    T_Article t_article = bdd.T_Article.Where(x => x.id == article.Id).FirstOrDefault();
+                    if (t_article != null)
+                    {
+                        t_article.id = article.Id;
+                        t_article.title = article.Title;
+                        t_article.image = article.Image;
+                        t_article.category_id = article.CategoryId;
+                        bdd.SaveChanges();
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+        public static DBO.Article GetArticle(long id)
+        {
+            using (IziWatchEntities bdd = new IziWatchEntities())
+            {
+                try {
+                    var query = from article in bdd.T_Article
+                                where article.id == id
+                                select article;
+
+                    if (query.Any())
+                    {
+                        return new DBO.Article()
+                        {
+                            Id = query.First().id,
+                            Title = query.First().title,
+                            Image = query.First().image,
+                            CategoryId = query.First().category_id
+                        };
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static List<DBO.Article> GetListArticle()
+        {
+            using (IziWatchEntities bdd = new IziWatchEntities())
+            {
+                try {
+                    List<DBO.Article> listArticles = new List<DBO.Article>();
+
+                    var query = from article in bdd.T_Article
+                                select article;
+
+                    foreach (var element in query)
+                    {
+                        listArticles.Add(new DBO.Article()
+                        {
+                            Id = element.id,
+                            Title = element.title,
+                            Image = element.image,
+                            CategoryId = element.category_id
+                        });
+                    }
+                    return listArticles;
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
     }
 }
