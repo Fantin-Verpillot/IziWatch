08ef8400e81be471be8ab5e5d8e72d590655da69
MrLyfing <ilyfing@gmail.com>
2016-06-19 20:45:06 +0200

    FIX FUCKING CS PROJ OMG ..

---
+++ IziWatch/Controllers/ArticleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IziWatch.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            DBO.Article article = DataAccess.Article.GetArticle((long)id);
+            ViewBag.article = article;
+
+            if (article == null)
+                return HttpNotFound();
+
+            return View();
+        }
+    }
+}
