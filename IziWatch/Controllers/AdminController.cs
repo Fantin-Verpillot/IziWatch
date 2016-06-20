c95e605f19dfdf31ccf069d26114af48f05d5590
MrLyfing <ilyfing@gmail.com>
2016-06-20 03:19:41 +0200

    Admin controller

---
+++ IziWatch/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IziWatch.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateArticle()
+        {
+            return View();
+        }
+
+        public ActionResult CreateArticleFB()
+        {
+            return View();
+        }
+
+        public ActionResult CreateArticleTwitter()
+        {
+            return View();
+        }
+
+        public ActionResult HandleAccounts()
+        {
+            return View();
+        }
+    }
+}
