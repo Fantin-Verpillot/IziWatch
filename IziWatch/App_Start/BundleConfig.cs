0200c967018d48f1059a2ffae35307daa131b5c4
MrLyfing <ilyfing@gmail.com>
2016-07-01 19:55:07 +0200

    bootstrap new theme

--- IziWatch/App_Start/BundleConfig.cs
+++ IziWatch/App_Start/BundleConfig.cs
@@ -23,8 +23,13 @@ namespace IziWatch
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            /*
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/site.css"));*/
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css"));
         }
     }
